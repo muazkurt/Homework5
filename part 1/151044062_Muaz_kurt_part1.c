feb3202fbed9096ee67cabf23422fa38ab86f257
Muaz Kurt <muazkurt@gmail.com>
2016-11-14 19:28:55 +0300

    Adding the function headers

--- part 1/151044062_Muaz_kurt_part1.c
+++ part 1/151044062_Muaz_kurt_part1.c
@@ -1,5 +1,70 @@
 #include <stdio.h>
 
+void sfrac_simplify(char *n)
+{
+
+	return;
+}
+
+char *sfrac_add(char *n1, char *n2)
+{
+
+
+	return n1;
+}
+
+char *sfrac_sub(char *n1, char *n2)
+{
+
+	return n1;
+}
+
+char *sfrac_negate(char *n)
+{
+
+
+	return n;
+}
+
+char *sfrac_mult(char *n1, char *n2)
+{
+
+
+	return n1;
+}
+
+char *sfrac_div(char *n1, char *n2)
+{
+
+
+	return n1;
+}
+
+char *sfrac_fromdouble(double x)
+{
+
+
+	return n1;
+}
+
+double sfrac_todouble(char *x)
+{
+
+	return 0.0;
+}
+
+void sfrac_print(
+	char *a1, char *n1,
+	char *a2, char *n2,
+	char *a3, char *n3,
+	char *a4)
+{
+
+
+	return;
+}
+
+
 void main()
 {
 
