7d211511bf45649347976cb8c842557293a4babc
Muaz Kurt <muazkurt@gmail.com>
2016-11-17 16:22:18 +0300

    sfrac_simplfy tamamlandı pompiiilllliiii

--- part 1/151044062_Muaz_kurt_part1.c
+++ part 1/151044062_Muaz_kurt_part1.c
@@ -1,33 +1,12 @@
 #include <stdio.h>
 #include <string.h>	
 #include <math.h>
+#include "sfrac.h"
 
-char *sfrac_simplify(char *n)
-{
-	int x, y, i;
-	x = sfrac_todouble(strtok(n, "/"));
-	y = sfrac_todouble(strtok(NULL, "/"));
-	for (i = 1; i < x && i < y; ++i)
-	{
-		if (x % i == 0 && y % i == 0)
-		{
-			x /= i;
-			y /= i;
-		}
-	}
-	/*
-
-	strcat(strcat(strcpy(n,sfrac_fromdouble(x)),'/'),sfrac_fromdouble(y))
-	*/
-
-
-	return n;//strcat(strcat(strcpy(n, x), '/'), "%d", y);
-}
 
 void main()
 {
-	double c;
-	c = 1/3;
-	printf("%s", sfrac_fromdouble(c));
+	char *c = "8/4";
+	sfrac_simplify(c);
 	return;
 }
--- part 1/sfrac.h
+++ part 1/sfrac.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string.h>
 #define MaxPowerer 8
+//my own functions
 int power(int i, int y)
 {
 	int x, total = 1;
@@ -9,190 +10,201 @@ int power(int i, int y)
 	return total;
 }
 
-
-char *sfrac_simplify(char *n)
+void simplifier(int *first, int *sec)
 {
-	int x, y, i;
-	x = sfrac_todouble(strtok(n, "/"));
-	y = sfrac_todouble(strtok(NULL, "/"));
-	for (i = 1; i < x && i < y; ++i)
+	int i, lessone = *first;
+	if (*first > *sec)
+		lessone = *sec;
+	for (i = 2; i < lessone; ++i)
 	{
-		if (x % i == 0 && y % i == 0)
+		if (*first%i == 0 && *sec %i == 0)
 		{
-			x /= i;
-			y /= i;
+			*first /= i;
+			*sec /= i;
+			i -= 1;
 		}
 	}
-	/*
-	
-	strcat(strcat(strcpy(n,sfrac_fromdouble(x)),'/'),sfrac_fromdouble(y))
-	*/
-	
-	
-	return n;//strcat(strcat(strcpy(n, x), '/'), "%d", y);
+	return;
 }
 
-char *sfrac_add(char *n1, char *n2)
-{
-	double x, y;
-	x = sfrac_todouble(n1);
-	y = sfrac_todouble(n2);
-	x += y;
-	/*
-	char çevirip return etme kýsmý
-	
-	*/
-	
-	return n1;
-}
-
-char *sfrac_sub(char *n1, char *n2)
-{
-	double x, y;
-	x = sfrac_todouble(n1);
-	y = sfrac_todouble(n2);
-	x -= y;
-	/*
-	char çevirip return etme kýsmý
-
-	*/
-	return n1;
-}
-
-char *sfrac_negate(char *n)
-{
-	char *m;
-	m[0] = '-';
-	if (n[0] == '-')
-		return strcpy(n, strcpy(&m[1], n));
-	return n;
-}
-
-char *sfrac_mult(char *n1, char *n2)
-{
-	double x, y;
-	x = sfrac_todouble(n1);
-	y = sfrac_todouble(n2);
-	x *= y;
-	/*
-	char çevirip return etme kýsmý
-
-	*/
-
-	return n1;
-}
-
-char *sfrac_div(char *n1, char *n2)
-{
-	double x, y;
-	x = sfrac_todouble(n1);
-	y = sfrac_todouble(n2);
-	x /= y;
-	/*
-	char çevirip return etme kýsmý
-
-	*/
-
-	return n1;
-}
-
-#define MaxPowerer 8
-
-char *sfrac_fromdouble(double x)
-{
-	char *n;
-	int i = 0, pre, a, j, fortop, q;
-	n[i++] = "_";
 
 
-	a = x - (int)x;
-	a *= pow(10, MaxPowerer);
-	if (a == 0)
-	{
-		strcat(n, "/1");
-		for (pre = (int)x; pre > 9; pre /= 10)
-		{
-			printf("%d", pre);
-			n[i++] = pre % 10;
-		}
+//my own functions end
 
-		for (j = 0; j < i; ++j)
-		{
-			pre = n[i];
-			n[i] = n[j];
-			n[j] = pre;
-			--i;
-		}
-		sfrac_simplify(x);
-	}
-	else
-	{
-		for (i = 0; a % 10 == 0; i++)
-			a /= 10;
-		i = MaxPowerer - i;
-		q = pow(10, MaxPowerer);
-		for (j = 0; a % 5 == 0 && (int)x % 5 == 2 && j < i; j++)
-		{
-			a /= 5;
-			x /= 5;
-			q /= 5;
-		}
-		for (fortop = 0; a % 2 == 0 && (int)x % 2 == 0 && fortop < i; ++fortop)
-		{
-			a /= 2;
-			x /= 2;
-			q /= 2;
-		}
-		for (pre = (int)x; pre > 9; pre /= 10)
-		{
-			printf("%d", pre);
-			n[i++] = pre % 10;
-		}
-
-		for (j = 0; j < i; ++j)
-		{
-			pre = n[i];
-			n[i] = n[j];
-			n[j] = pre;
-			--i;
-		}
-		//	strcat(strcat(n, n/*("%d", a)*/), ("/10^%d", (MaxPowerer - i)));
-
-	}
-	strtok(n, "_");
-
-	return strtok(NULL, "_");
-}
 
 double sfrac_todouble(char *x)
 {
-	double upper = 0, downer = 0, i;
+	int upper = 0, downer = 0, i;
 
 	if (strlen(x) == strlen(strtok(x, "/")))
 	{
-		sscanf(x, "%lf", &upper);
+		sscanf(x, "%d", &upper);
 		return upper;
 	}
 
-	sscanf(x, "%lf", &upper);
+	sscanf(x, "%d", &upper);
 
-	sscanf(strtok(NULL, "/"), "%lf", &downer);
+	sscanf(strtok(NULL, "/"), "%d", &downer);
 
 	if (downer != 0)
-		return upper / downer;
-
+		return (upper / downer);
 
 
 	return -1;
 }
 
-void sfrac_print(
-	char *a1, char *n1,
-	char *a2, char *n2,
-	char *a3, char *n3,
-	char *a4)
+
+char *sfrac_simplify(char *n)
 {
+	int x, y, i, lessone;
+	sscanf(n, "%d/%d", &x, &y);
+	simplifier(&x, &y);
 
+	//update n with sfrac_fromdouble(x,y) vsvs
 
-	return;
+	return n;
 }
+
+//char *sfrac_add(char *n1, char *n2)
+//{
+//	double x, y;
+//	x = sfrac_todouble(n1);
+//	y = sfrac_todouble(n2);
+//	x += y;
+//	/*
+//	char çevirip return etme kýsmý
+//	
+//	*/
+//	
+//	return n1;
+//}
+//
+//char *sfrac_sub(char *n1, char *n2)
+//{
+//	double x, y;
+//	x = sfrac_todouble(n1);
+//	y = sfrac_todouble(n2);
+//	x -= y;
+//	/*
+//	char çevirip return etme kýsmý
+//
+//	*/
+//	return n1;
+//}
+//
+//char *sfrac_negate(char *n)
+//{
+//	char *m;
+//	m[0] = '-';
+//	if (n[0] == '-')
+//		return strcpy(n, strcpy(&m[1], n));
+//	return n;
+//}
+//
+//char *sfrac_mult(char *n1, char *n2)
+//{
+//	double x, y;
+//	x = sfrac_todouble(n1);
+//	y = sfrac_todouble(n2);
+//	x *= y;
+//	/*
+//	char çevirip return etme kýsmý
+//
+//	*/
+//
+//	return n1;
+//}
+//
+//char *sfrac_div(char *n1, char *n2)
+//{
+//	double x, y;
+//	x = sfrac_todouble(n1);
+//	y = sfrac_todouble(n2);
+//	x /= y;
+//	/*
+//	char çevirip return etme kýsmý
+//
+//	*/
+//
+//	return n1;
+//}
+//
+//
+//char *sfrac_fromdouble(double x)
+//{
+//	char *n;
+//	int i = 0, pre, a, j, fortop, q;
+//	n[i++] = "_";
+//
+//
+//	a = x - (int)x;
+//	a *= pow(10, MaxPowerer);
+//	if (a == 0)
+//	{
+//		strcat(n, "/1");
+//		for (pre = (int)x; pre > 9; pre /= 10)
+//		{
+//			printf("%d", pre);
+//			n[i++] = pre % 10;
+//		}
+//
+//		for (j = 0; j < i; ++j)
+//		{
+//			pre = n[i];
+//			n[i] = n[j];
+//			n[j] = pre;
+//			--i;
+//		}
+//		sfrac_simplify(x);
+//	}
+//	else
+//	{
+//		for (i = 0; a % 10 == 0; i++)
+//			a /= 10;
+//		i = MaxPowerer - i;
+//		q = pow(10, MaxPowerer);
+//		for (j = 0; a % 5 == 0 && (int)x % 5 == 2 && j < i; j++)
+//		{
+//			a /= 5;
+//			x /= 5;
+//			q /= 5;
+//		}
+//		for (fortop = 0; a % 2 == 0 && (int)x % 2 == 0 && fortop < i; ++fortop)
+//		{
+//			a /= 2;
+//			x /= 2;
+//			q /= 2;
+//		}
+//		for (pre = (int)x; pre > 9; pre /= 10)
+//		{
+//			printf("%d", pre);
+//			n[i++] = pre % 10;
+//		}
+//
+//		for (j = 0; j < i; ++j)
+//		{
+//			pre = n[i];
+//			n[i] = n[j];
+//			n[j] = pre;
+//			--i;
+//		}
+//		//	strcat(strcat(n, n/*("%d", a)*/), ("/10^%d", (MaxPowerer - i)));
+//
+//	}
+//	strtok(n, "_");
+//
+//	return strtok(NULL, "_");
+//}
+//
+//
+//
+//void sfrac_print(
+//	char *a1, char *n1,
+//	char *a2, char *n2,
+//	char *a3, char *n3,
+//	char *a4)
+//{
+//
+//
+//	return;
+//}
